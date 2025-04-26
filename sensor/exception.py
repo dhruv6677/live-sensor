6452335e47a812c084f132144bff456e34ebfc93
dhruv <progammerdhruv@gmail.com>
2025-04-26 22:28:37 +0530

    update exception file main file logger

--- sensor/exception.py
+++ sensor/exception.py
@@ -2,15 +2,29 @@ import sys
 import os
 
 # store error in  line no, filename, error
-def error_message_detail():
-    _,_,exc_tb = sys.exc_error_detail.exc_info()
+
+
+def error_message_detail(error, error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info()
+    filename = exc_tb.tb_frame.f_code.co_filename
+
+    
+    error_message = "error occured and the file name is [{0}] and the linenumber is [{1} and error is ({2})]".format(
+    filename, exc_tb.tb_lineno, str(error))
+    return error_message
 
 # store error in  line no, filename, error
 
 class SensorException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
+        
 
+        self.error_message = error_message_detail(error_message,error_detail=error_detail)
     def __str__(self):
         return self.error_message
+     
+
+      
+    
         
---
+++ sensor/logger.py
@@ -0,0 +1,14 @@
+import logging
+import os
+import sys
+from datetime import datetime
+
+logging.basicConfig(
+filename=,
+format=,
+level=logging.INFO()
+
+
+
+
+)
